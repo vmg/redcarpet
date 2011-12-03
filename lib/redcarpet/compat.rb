83bd6b387bfb522d78e4a398f0f2cdfb7160f199
Vicent Marti <tanoku@gmail.com>
2011-12-03 23:12:42 +0100

    Give up on everything that is right

--- lib/redcarpet/compat.rb
+++ lib/redcarpet/compat.rb
@@ -1,21 +1,3 @@
 require 'redcarpet'
 
-# Compatibility class;
-# Creates a instance of Redcarpet with the RedCloth
-# API. This instance has no extensions enabled whatsoever,
-# and no accessors to change this. 100% pure, standard
-# Markdown.
-class RedcarpetCompat
-  attr_accessor :text
-
-  def initialize(text, *_dummy)
-    @text = text
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-  end
-
-  def to_html(*_dummy)
-    @markdown.render(@text)
-  end
-end
-
 Markdown = RedcarpetCompat unless defined? Markdown
