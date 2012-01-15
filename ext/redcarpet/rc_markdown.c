3754d9a8f832dfec1457cad292ce7bfb5ae8e518
Loren Segal <lsegal@soen.ca>
2012-01-15 16:56:20 -0500

    Fix broken encoding test when input is not UTF-8
    Fixes the failing test #test_should_return_string_in_same_encoding_not_in_utf8
    in test/redcarpet_test.rb

--- ext/redcarpet/rc_markdown.c
+++ ext/redcarpet/rc_markdown.c
@@ -127,7 +127,7 @@ static VALUE rb_redcarpet_md_render(VALUE self, VALUE text)
 		markdown);
 
 	/* build the Ruby string */
-	text = redcarpet_str_new(output_buf->data, output_buf->size);
+	text = redcarpet_str_new(output_buf->data, output_buf->size, rb_enc_get(text));
 
 	bufrelease(output_buf);
 
