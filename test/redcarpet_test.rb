ef0926b0f13c0cd4d5556cc94b74007d7b8a50d6
Vicent Marti <tanoku@gmail.com>
2012-10-11 15:40:04 +0200

    Fix intra emphasis issues

--- test/redcarpet_test.rb
+++ test/redcarpet_test.rb
@@ -336,6 +336,14 @@ text
     rd = render_with({:space_after_headers => true}, "#123 a header yes\n")
     assert rd !~ /<h1>/
   end
+
+  def test_proper_intra_emphasis
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :no_intra_emphasis => true)
+    assert render_with({:no_intra_emphasis => true}, "http://en.wikipedia.org/wiki/Dave_Allen_(comedian)") !~ /<em>/
+    assert render_with({:no_intra_emphasis => true}, "this fails: hello_world_") !~ /<em>/
+    assert render_with({:no_intra_emphasis => true}, "this also fails: hello_world_#bye") !~ /<em>/
+    assert render_with({:no_intra_emphasis => true}, "this works: hello_my_world") !~ /<em>/
+  end
 end
 
 class CustomRenderTest < Test::Unit::TestCase
