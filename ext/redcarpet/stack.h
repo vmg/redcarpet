3793253be526c45c479839e0a97667fee8f38bb0
Kenta Murata <mrkn@cookpad.com>
2012-12-18 14:24:30 +0900

    Add redcarpet_ prefix for stack_* functions

--- ext/redcarpet/stack.h
+++ ext/redcarpet/stack.h
@@ -13,14 +13,14 @@ struct stack {
 	size_t asize;
 };
 
-void stack_free(struct stack *);
-int stack_grow(struct stack *, size_t);
-int stack_init(struct stack *, size_t);
+void redcarpet_stack_free(struct stack *);
+int redcarpet_stack_grow(struct stack *, size_t);
+int redcarpet_stack_init(struct stack *, size_t);
 
-int stack_push(struct stack *, void *);
+int redcarpet_stack_push(struct stack *, void *);
 
-void *stack_pop(struct stack *);
-void *stack_top(struct stack *);
+void *redcarpet_stack_pop(struct stack *);
+void *redcarpet_stack_top(struct stack *);
 
 #ifdef __cplusplus
 }
