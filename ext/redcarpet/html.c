d3bc74393ca2ab629a58e30879a1f49d7ed5f4c2
Joel Rosenberg <joeldrosenberg@yahoo.com>
2013-01-25 14:18:55 -0800

    Adding options to prettify instead

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -121,10 +121,14 @@ rndr_blockcode(struct buf *ob, const struct buf *text, const struct buf *lang, v
 	if (ob->size) bufputc(ob, '\n');
 
 	if (lang && lang->size) {
-		size_t i;
-		BUFPUTSL(ob, "<pre><code class=\"prettyprint");
+		size_t i, cls;
+		BUFPUTSL(ob, "<pre><code class=\"");
+		if (render_flags & HTML_PRETTIFY) {
+			BUFPUTSL(ob, "prettyprint");
+			cls++;
+		}
 
-		for (i = 0; i < lang->size; ++i) {
+		for (i = 0, cls = 0; i < lang->size; ++i, ++cls) {
 			while (i < lang->size && isspace(lang->data[i]))
 				i++;
 
@@ -136,14 +140,16 @@ rndr_blockcode(struct buf *ob, const struct buf *text, const struct buf *lang, v
 				if (lang->data[org] == '.')
 					org++;
 
-				bufputc(ob, ' ');
+				if (cls) bufputc(ob, ' ');
 				escape_html(ob, lang->data + org, i - org);
 			}
 		}
 
 		BUFPUTSL(ob, "\">");
-	} else
+	} else if (render_flags & HTML_PRETTIFY)
 		BUFPUTSL(ob, "<pre><code class=\"prettyprint\">");
+	} else
+		BUFPUTSL(ob, "<pre><code>");
 
 	if (text)
 		escape_html(ob, text->data, text->size);
