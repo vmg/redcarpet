cbfc9958863eb7364d2c4c4ed4e49e6624be5d33
Joel Rosenberg <joeldrosenberg@yahoo.com>
2013-01-25 15:05:05 -0800

    Forgot about inline blocks

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -173,7 +173,11 @@ rndr_blockquote(struct buf *ob, const struct buf *text, void *opaque)
 static int
 rndr_codespan(struct buf *ob, const struct buf *text, void *opaque)
 {
-	BUFPUTSL(ob, "<code class=\"prettyprint\">");
+	struct html_renderopt *options = opaque;
+	if (options->flags & HTML_PRETTIFY)
+		BUFPUTSL(ob, "<code class=\"prettyprint\">");
+	else
+		BUFPUTSL(ob, "<code>");
 	if (text) escape_html(ob, text->data, text->size);
 	BUFPUTSL(ob, "</code>");
 	return 1;
