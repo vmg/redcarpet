vmg/redcarpet

commit 108e65df61732dc67aebe317a2e7baea959356ff

Generate new hash using html5 block names

ext/redcarpet/html_blocks.h
@@ -1,4 +1,4 @@
-/* C code produced by gperf version 3.0.3 */
+/* C code produced by gperf version 3.0.4 */
 /* Command-line: gperf -N find_block_tag -H hash_block_tag -C -c -E --ignore-case html_block_names.txt  */
 /* Computed positions: -k'1-2' */
 
@@ -29,7 +29,7 @@
 error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gnu-gperf@gnu.org>."
 #endif
 
-/* maximum key range = 37, duplicates = 0 */
+/* maximum key range = 51, duplicates = 0 */
 
 #ifndef GPERF_DOWNCASE
 #define GPERF_DOWNCASE 1
@@ -93,32 +93,32 @@ hash_block_tag (str, len)
 {
   static const unsigned char asso_values[] =
     {
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-       8, 30, 25, 20, 15, 10, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38,  0, 38,  0, 38,
-       5,  5,  5, 15,  0, 38, 38,  0, 15, 10,
-       0, 38, 38, 15,  0,  5, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38,  0, 38,
-       0, 38,  5,  5,  5, 15,  0, 38, 38,  0,
-      15, 10,  0, 38, 38, 15,  0,  5, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
-      38, 38, 38, 38, 38, 38, 38
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      35, 30, 25, 20, 15, 10, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 15,  0, 25, 15, 53,
+       5, 10, 15, 25,  0, 53, 53,  0, 25,  5,
+       5, 53, 53,  0, 10,  0, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 15,  0, 25,
+      15, 53,  5, 10, 15, 25,  0, 53, 53,  0,
+      25,  5,  5, 53, 53,  0, 10,  0, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
+      53, 53, 53, 53, 53, 53, 53
     };
   register int hval = len;
 
@@ -136,7 +136,7 @@ hash_block_tag (str, len)
 
 #ifdef __GNUC__
 __inline
-#ifdef __GNUC_STDC_INLINE__
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
 __attribute__ ((__gnu_inline__))
 #endif
 #endif
@@ -147,47 +147,59 @@ find_block_tag (str, len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 24,
+      TOTAL_KEYWORDS = 33,
       MIN_WORD_LENGTH = 1,
       MAX_WORD_LENGTH = 10,
-      MIN_HASH_VALUE = 1,
-      MAX_HASH_VALUE = 37
+      MIN_HASH_VALUE = 2,
+      MAX_HASH_VALUE = 52
     };
 
   static const char * const wordlist[] =
     {
-      "",
-      "p",
-      "dl",
-      "div",
-      "math",
-      "table",
-      "",
+      "", "",
       "ul",
-      "del",
-      "form",
+      "",
+      "math",
+      "style",
+      "p",
+      "ol",
+      "pre",
+      "",
       "blockquote",
       "figure",
-      "ol",
+      "section",
       "fieldset",
-      "",
-      "h1",
-      "",
-      "h6",
-      "pre",
+      "form",
+      "table",
+      "footer",
+      "dl",
+      "div",
       "", "",
       "script",
-      "h5",
-      "noscript",
+      "article",
+      "del",
+      "", "",
+      "header",
+      "h6",
+      "nav",
       "",
-      "style",
-      "iframe",
-      "h4",
+      "aside",
+      "canvas",
+      "h5",
       "ins",
-      "", "", "",
+      "", "",
+      "hgroup",
+      "h4",
+      "noscript",
+      "", "",
+      "iframe",
       "h3",
+      "",
+      "abbr",
+      "", "",
+      "h2",
       "", "", "", "",
-      "h2"
+      "h1"
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)

commit 0f7f50056c75e61ea6c9867b07b796a0f64ab564

Consider the `script` tag as a block-level element
This commit addresses a regression introduced in 108e65df that made the
`script` tag to be considered as a non-block-level element so it was
wrapped inside a paragraph.

Fixes #440.

ext/redcarpet/html_blocks.h
@@ -1,5 +1,6 @@
 /* C code produced by gperf version 3.0.4 */
 /* Command-line: gperf -N find_block_tag -H hash_block_tag -C -c -E --ignore-case html_block_names.txt  */
+/* See http://git.io/RN0ncw for the list of recognized elements */
 /* Computed positions: -k'1-2' */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
@@ -99,10 +100,10 @@ hash_block_tag (str, len)
       68, 68, 68, 68, 68, 68, 68, 68, 68, 68,
       68, 68, 68, 68, 68, 68, 68, 68, 68, 68,
       55, 50, 45, 40, 35, 30, 68, 68, 68, 68,
-      68, 68, 68, 68, 68, 15, 10,  5, 15, 15,
+      68, 68, 68, 68, 68, 15, 10, 15, 15, 15,
        0, 20, 10, 10,  5, 68, 68,  0, 20, 25,
        0, 68, 68,  0, 25,  0, 15, 68, 68, 68,
-      68, 68, 68, 68, 68, 68, 68, 15, 10,  5,
+      68, 68, 68, 68, 68, 68, 68, 15, 10, 15,
       15, 15,  0, 20, 10, 10,  5, 68, 68,  0,
       20, 25,  0, 68, 68,  0, 25,  0, 15, 68,
       68, 68, 68, 68, 68, 68, 68, 68, 68, 68,
@@ -147,7 +148,7 @@ find_block_tag (str, len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 40,
+      TOTAL_KEYWORDS = 41,
       MIN_WORD_LENGTH = 1,
       MAX_WORD_LENGTH = 10,
       MIN_HASH_VALUE = 1,
@@ -175,18 +176,19 @@ find_block_tag (str, len)
       "del",
       "",
       "blockquote",
-      "canvas",
+      "script",
       "article",
       "div",
-      "abbr",
+      "",
       "video",
       "hgroup",
       "ol",
       "noscript",
-      "", "", "",
+      "", "",
+      "canvas",
       "dd",
       "nav",
-      "",
+      "abbr",
       "audio",
       "iframe",
       "address",

test/html5_test.rb
@@ -57,4 +57,13 @@ EOE
     assert_renders section_expected, section
     assert_renders header_expected, header
   end
+
+  def test_script_tag_recognition
+    markdown = <<-Md
+<script type="text/javascript">
+  alert('Foo!');
+</script>
+Md
+    assert_renders markdown, markdown
+  end
 end