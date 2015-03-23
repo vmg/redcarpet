2becb645dc4bb62aff3f4e5ad46a891fc32c5bb5
Yuji Nakayama <nkymyj@gmail.com>
2015-03-24 02:35:50 +0900

    Tweak spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
 
   # This setting enables warnings. It's recommended, but in some cases may
   # be too noisy due to issues in dependencies.
-  config.warnings = true
+  # config.warnings = true
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
@@ -74,7 +74,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  # config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
